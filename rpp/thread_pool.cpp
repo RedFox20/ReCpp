286086b20eb25d23919cc8a4257c4719ce4f1ca2
Jorma <jorma.rebane@gmail.com>
2017-04-04 03:04:46 +0300

    Basic threadpool implementation

---
+++ rpp/thread_pool.cpp
@@ -0,0 +1,183 @@
+#include "thread_pool.h"
+#include <chrono>
+#include <cassert>
+#include <csignal>
+
+namespace rpp
+{
+    ///////////////////////////////////////////////////////////////////////////////
+
+    pool_task::~pool_task() noexcept
+    {
+        unique_lock<mutex> lock(mtx);
+        killed = true;
+        cv.notify_one();
+        th.detach();
+    }
+
+    void pool_task::run_task(int start, int end, const action<int, int>& newTask) noexcept
+    {
+        unique_lock<mutex> lock(mtx);
+        task        = newTask;
+        loopStart   = start;
+        loopEnd     = end;
+        taskRunning = true;
+        cv.notify_one();
+    }
+
+    void pool_task::wait(int timeoutMillis) noexcept
+    {
+        //printf("wait for (%d, %d)\n", loopStart, loopEnd);
+        while (taskRunning) // duplicate check is intentional (to avoid lock)
+        {
+            unique_lock<mutex> lock(mtx);
+            if (!taskRunning)
+                return;
+            if (timeoutMillis)
+            {
+                if (cv.wait_for(lock, chrono::milliseconds(timeoutMillis)) == cv_status::timeout)
+                    return;
+            }
+            else
+            {
+                cv.wait(lock);
+            }
+        }
+    }
+
+    void pool_task::run() noexcept
+    {
+        signal(SIGSEGV, [](int signal) { throw signal; }); // throw any segfaults from this thread
+
+        while (!killed)
+        {
+            if (wait_for_task())
+            {
+                try
+                {
+                    taskRunning = true;
+                    task(loopStart, loopEnd);
+                }
+                catch (int signal) 
+                {
+                    fprintf(stderr, "pool_task::run SIGSEGV!\n");
+                }
+                catch (const exception& e)
+                {
+                    fprintf(stderr, "pool_task::run unhandled exception: %s\n", e.what());
+                }
+                catch (...) // don't fail in any case
+                {
+                    fprintf(stderr, "pool_task::run unhandled exception!\n");
+                }
+
+                if (killed)
+                    return;
+            }
+            taskRunning = false;
+            cv.notify_one();
+        }
+    }
+
+    bool pool_task::wait_for_task() noexcept
+    {
+        unique_lock<mutex> lock(mtx);
+        cv.wait(lock);
+        return (bool)task; // got task?
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////////
+
+    thread_pool thread_pool::global;
+
+    thread_pool::~thread_pool() noexcept
+    {
+        // defined destructor to prevent agressive inlining
+    }
+
+    int thread_pool::active_tasks() noexcept
+    {
+        int active = 0;
+        for (auto& task : tasks) 
+            if (task->running()) ++active;
+        return active;
+    }
+
+    int thread_pool::idle_tasks() noexcept
+    {
+        int idle = 0;
+        for (auto& task : tasks)
+            if (!task->running()) ++idle;
+        return idle;
+    }
+
+    int thread_pool::total_tasks() noexcept
+    {
+        return (int)tasks.size();
+    }
+
+    int thread_pool::clear_idle_tasks() noexcept
+    {
+        int cleared = 0;
+        for (int i = 0; i < (int)tasks.size(); ++i)
+        {
+            if (!tasks[i]->running())
+            {
+                swap(tasks[i], tasks.back()); // erase_swap_last pattern
+                tasks.pop_back();
+                ++cleared;
+            }
+        }
+        return cleared;
+    }
+
+    pool_task* thread_pool::new_task() noexcept
+    {
+        tasks.emplace_back(make_unique<pool_task>());
+        return tasks.back().get();
+    }
+
+    pool_task* thread_pool::next_task(size_t& poolIndex) noexcept
+    {
+        for (; poolIndex < tasks.size(); ++poolIndex)
+        {
+            pool_task* task = tasks[poolIndex].get();
+            if (!task->running())
+                return task;
+        }
+        return new_task();
+    }
+
+    void thread_pool::parallel_for(int rangeStart, int rangeEnd, 
+                                   const action<int, int>& rangeTask) noexcept
+    {
+        const int physicalCores = (int)thread::hardware_concurrency();
+        assert(physicalCores != 0);
+
+        const int range = rangeEnd - rangeStart;
+        if (range <= 0)
+            return;
+
+        const int cores = range < physicalCores ? range : physicalCores;
+        const int len = range / cores;
+
+        pool_task** active = (pool_task**)_alloca(sizeof(pool_task*) * cores);
+        {
+            lock_guard<mutex> lock(poolmutex);
+            size_t poolIndex = 0;
+            for (int i = 0; i < cores; ++i)
+            {
+                const int start = i * len;
+                const int end   = i == cores - 1 ? rangeEnd : start + len;
+
+                pool_task* task = next_task(poolIndex);
+                active[i] = task;
+                task->run_task(start, end, rangeTask);
+            }
+        }
+
+        for (int i = 0; i < cores; ++i)
+            active[i]->wait();
+    }
+}
---
+++ rpp/thread_pool.h
@@ -0,0 +1,143 @@
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+namespace rpp
+{
+    using namespace std;
+
+    // Optimized Action delegate, using the 'Fastest Possible C++ Delegates' method
+    // This supports lambdas by grabbing a pointer to the lambda instance,
+    // so its main intent is to be used during blocking calls like parallel_for
+    // It's completely useless for async callbacks that expect lambdas to be stored
+    template<class... TArgs> class action
+    {
+        using Func = void(*)(void* callee, TArgs...);
+        void* Callee;
+        Func Function;
+
+        template <class T, void (T::*TMethod)(TArgs...)>
+        static void methodCaller(void* callee, TArgs... args)
+        {
+            T* p = static_cast<T*>(callee);
+            return (p->*TMethod)(args...);
+        }
+
+        template<class T, void (T::*TMethod)(TArgs...)const>
+        static void methodCaller(void* callee, TArgs... args)
+        {
+            const T* p = static_cast<T*>(callee);
+            return (p->*TMethod)(args...);
+        }
+
+    public:
+        action() : Callee(nullptr), Function(nullptr) {}
+        action(void* callee, Func function) : Callee(callee), Function(function) {}
+
+        template<class T, void (T::*TMethod)(TArgs...)>
+        static action from_function(T* callee)
+        {
+            return action((void*)callee, &methodCaller<T, TMethod>);
+        }
+
+        template<class T, void (T::*TMethod)(TArgs...)const>
+        static action from_function(const T* callee)
+        {
+            return action((void*)callee, &methodCaller<T, TMethod>);
+        }
+
+        inline void operator()(TArgs... args) const
+        {
+            (*Function)(Callee, args...);
+        }
+
+        operator bool() const { return Callee != nullptr; }
+    };
+
+
+    class pool_task
+    {
+        mutex mtx;
+        condition_variable cv;
+        thread th = thread(&pool_task::run, this);
+        action<int, int> task;
+        int loopStart;
+        int loopEnd;
+        volatile bool taskRunning = false;
+        volatile bool killed = false;
+
+    public:
+
+        bool running() const noexcept { return taskRunning; }
+
+        ~pool_task() noexcept;
+
+        // assigns a new task to run
+        // undefined behaviour if called when running (parallel_for already manages this)
+        void run_task(int start, int end, const action<int, int>& newTask) noexcept;
+
+        // wait for task to finish
+        void wait(int timeoutMillis = 0/*0=no timeout*/) noexcept;
+
+    private:
+        void run();
+        bool wait_for_task();
+    };
+
+
+    /**
+     * A generic thread pool that can be used to group and control pool lifetimes
+     * By default a global thread_pool is also available
+     */
+    class thread_pool
+    {
+        mutex poolmutex;
+        vector<unique_ptr<pool_task>> tasks;
+
+    public:
+
+        // the default global thread pool
+        static thread_pool global;
+
+        ~thread_pool() noexcept;
+
+        // number of thread pool tasks that are currently running
+        int active_tasks() noexcept;
+
+        // number of thread pool tasks that are in idle state
+        int idle_tasks() noexcept;
+
+        // number of running + idle tasks in this threadpool
+        int total_tasks() noexcept;
+
+        // if you're completely done with the thread pool, simply call this to clean up the threads
+        // returns the number of tasks cleared
+        int clear_idle_tasks() noexcept;
+
+        // always creates a brand new task that is registered in the pool (and will get reused)
+        pool_task* new_task() noexcept;
+
+        pool_task* next_task(size_t& poolIndex) noexcept;
+
+        void parallel_for(int rangeStart, int rangeEnd, const action<int, int>& rangeTask) noexcept;
+
+        template<class Func> 
+        void parallel_for(int rangeStart, int rangeEnd, const Func& func) noexcept
+        {
+            parallel_for(rangeStart, rangeEnd, 
+                action<int, int>::from_function<Func, &Func::operator()>(&func));
+        }
+    };
+
+    // runs parallel_for on the default global thread pool
+    template<class Func> 
+    inline void parallel_for(int rangeStart, int rangeEnd, const Func& func) noexcept
+    {
+        thread_pool::global.parallel_for(rangeStart, rangeEnd,
+            action<int, int>::from_function<Func, &Func::operator()>(&func));
+    }
+
+
+
+} // namespace rpp
