34df0a6238d595dcc18a80cc66ce843b04d58d3f
Jorma <jorma.rebane@gmail.com>
2017-10-19 18:23:48 +0300

    Improved tests output

--- rpp/tests.cpp
+++ rpp/tests.cpp
@@ -319,7 +319,7 @@ namespace rpp
                     if (t->auto_run) { // only consider disabling auto_run tests
                         t->test_enabled = disabled.find(t->name) == disabled.end();
                         if (!t->test_enabled)
-                            consolef(ConsoleColor::Red, "Disabled test %s\n", t->name.to_cstr());
+                            consolef(Red, "  Disabled %s\n", t->name.to_cstr());
                     }
                 }
             }
@@ -328,13 +328,16 @@ namespace rpp
                 for (test* t : all_tests()) { // enable whatever was requested
                     t->test_enabled = enabled.find(t->name) != enabled.end();
                     if (t->test_enabled)
-                        consolef(ConsoleColor::Green, "Enabled test %s\n", t->name.to_cstr());
+                        consolef(Green, "  Enabled %s\n", t->name.to_cstr());
                 }
             }
         }
         else
         {
             consolef(Green, "Running all auto-run tests\n");
+            for (test* t : all_tests())
+                if (!t->auto_run && !t->test_enabled)
+                    consolef(Yellow, "  Disabled NoAutoRun %s\n", t->name.to_cstr());
         }
 
         // run all the marked tests
