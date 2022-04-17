34fa5fdea475eca8c84808214bd01e910732a7cb
RedFox <jorma.rebane@gmail.com>
2022-04-18 02:09:05 +0300

    test: fix timer test issues

--- tests/test_timer.cpp
+++ tests/test_timer.cpp
@@ -1,6 +1,7 @@
 #include <rpp/timer.h>
 #include <rpp/tests.h>
 #include <thread>
+#include <chrono>
 using namespace rpp;
 using namespace std::literals;
 
@@ -12,21 +13,31 @@ TestImpl(test_timer)
     {
     }
 
+    static void sleep_for(double seconds)
+    {
+        using clock = std::chrono::high_resolution_clock;
+        static constexpr std::chrono::duration<double> MIN_SLEEP_DURATION {0};
+        clock::time_point start = clock::now();
+        while (std::chrono::duration<double>(clock::now() - start).count() < seconds) {
+            std::this_thread::sleep_for(MIN_SLEEP_DURATION);
+        }
+    }
+
     TestCase(basic_timer)
     {
         Timer t;
-        std::this_thread::sleep_for(100ms);
+        sleep_for(0.1);
         double elapsed = t.elapsed();
-        //println("100ms sleep time:", elapsed, "seconds");
+        print_info("100ms sleep time: %f seconds\n", elapsed);
         AssertThat(0.1 <= elapsed && elapsed <= 0.1 + sigma_s, true);
     }
 
     TestCase(basic_timer_ms)
     {
         Timer t;
-        std::this_thread::sleep_for(100ms);
+        sleep_for(0.1);
         double elapsed = t.elapsed_ms();
-        //println("100ms sleep time:", elapsed, "milliseconds");
+        print_info("100ms sleep time: %f milliseconds\n", elapsed);
         AssertThat(100.0 <= elapsed && elapsed <= 100.0 + sigma_ms, true);
     }
 
@@ -41,12 +52,12 @@ TestImpl(test_timer)
         AssertThat(sw.started(), true);
         AssertThat(sw.stopped(), false);
 
-        std::this_thread::sleep_for(100ms);
+        sleep_for(0.1);
 
         sw.stop();
         AssertThat(sw.started(), true);
         AssertThat(sw.stopped(), true);
-        //println("100ms stopwatch time:", sw.elapsed(), "seconds");
+        print_info("100ms stopwatch time: %f seconds\n", sw.elapsed());
         AssertThat(0.1 <= sw.elapsed() && sw.elapsed() <= 0.1 + sigma_s, true);
         AssertThat(sw.elapsed(), sw.elapsed()); // ensure time is stable after stop
 
