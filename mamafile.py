7b77fa11c1d0140345bb868a608b8d13003e50c2
Jorma Rebane <jorma.rebane@gmail.com>
2023-03-01 13:04:21 +0200

    build: export syslibs for oclea and raspi builds

--- mamafile.py
+++ mamafile.py
@@ -13,14 +13,17 @@ class ReCpp(mama.BuildTarget):
     def package(self):
         self.export_libs('.', ['ReCpp.lib', 'ReCpp.a'])
         self.export_includes('.')  # So: #include <rpp/strview.h>
-        if self.linux:
+        if self.raspi or self.oclea:
+            self.export_syslib('dl')
+            self.export_syslib('rt')
+        elif self.linux:
             self.export_syslib('dl')
             self.export_syslib('dw', 'libdw-dev')
             self.export_syslib('rt')
-        if self.android:
+        elif self.android:
             self.export_syslib('android')
             self.export_syslib('log')
-        if self.macos or self.ios:
+        elif self.macos or self.ios:
             self.export_syslib('-framework Foundation')
 
     def test(self, args):
