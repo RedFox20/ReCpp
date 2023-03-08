ef9f80510aa776d058fe0fa22cc519657d06a10b
Jorma Rebane <jorma.rebane@gmail.com>
2023-03-08 23:37:00 +0200

    updated mamafile

--- mamafile.py
+++ mamafile.py
@@ -29,13 +29,6 @@ class ReCpp(mama.BuildTarget):
     def test(self, args):
         if 'nogdb' in args:
             args = args.replace('nogdb', '')
-            command = f'cd {self.source_dir("bin")} && ./RppTests {args}'
-            # self.run_program(self.source_dir('bin'), self.source_dir(f'bin/RppTests {args}'))
+            self.run_program(self.source_dir('bin'), self.source_dir(f'bin/RppTests {args}'))
         else:
-            gdb =  f'gdb -batch -ex=r -ex=bt -ex=q --args {self.source_dir("bin/RppTests")}'
-            command = f'cd {self.source_dir("bin")} && {gdb} {args}'
-            # self.run_program(self.source_dir('bin'), gdb)
-            # self.gdb(f'bin/RppTests {args}', src_dir=True)
-        # TODO: fix subprocess execution (currently broken)
-        retcode = os.system(command)
-        if retcode != 0: raise Exception(f'{command} failed with return code {retcode}')
+            self.gdb(f"bin/RppTests {args}", src_dir=True)
