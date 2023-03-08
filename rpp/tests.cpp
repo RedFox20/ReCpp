3a2e2b008ee4550fd4cf3880e6a6375ddb32fb65
Jorma <jorma.rebane@gmail.com>
2023-03-09 01:04:30 +0200

    tests: change return codes to be more specific

--- rpp/tests.cpp
+++ rpp/tests.cpp
@@ -712,12 +712,12 @@ namespace rpp
                 else        consolef(Red, "    %s::%s: %s\n",    f.testname.data(), f.testcase.data(), f.message.data());
             }
             consolef(Default, "\n");
-            return -1;
+            return failed;
         }
         if (numTests <= 0)
         {
             consolef(Yellow, "\nNOTE: No tests were run! (out of %d available)\n\n", (int)state().global_tests.size());
-            return -1;
+            return 1;
         }
         if (numTests == 1) consolef(Green, "\nSUCCESS: Test passed!\n\n");
         else               consolef(Green, "\nSUCCESS: All %d tests passed!\n\n", numTests);
@@ -758,7 +758,7 @@ namespace rpp
         catch (const std::exception& e)
         {
             consolef(Red, "Unhandled exception while running tests: %s\n", e.what());
-            return -1;
+            return 1;
         }
 
         int returnCode = print_final_summary(results);
