25961c0b7c8c249f0ece867aea98bec911773963
Jorma Rebane <jorma.rebane@gmail.com>
2023-04-25 20:53:15 +0300

    feature: improved concurrent_queue and added full unit test coverage

--- rpp/concurrent_queue.h
+++ rpp/concurrent_queue.h
@@ -1,5 +1,6 @@
 /**
- * Basic concurrent queue with several synchronization helpers, Copyright (c) 2017-2018, Jorma Rebane
+ * Basic concurrent queue with several synchronization helpers, 
+ * Copyright (c) 2017-2018, 2023, Jorma Rebane
  * Distributed under MIT Software License
  */
 #pragma once
@@ -10,7 +11,11 @@
 namespace rpp
 {
     /**
-     * @note This is not optimized for speed
+     * Provides a simple thread-safe queue with several synchronization helpers
+     * as a simple way to write thread-safe code between multiple worker threads.
+     * 
+     * @note This is not optimized for speed, but has acceptable performance
+     *       and due to its simplicity it won't randomly deadlock on you.
      */
     template<class T>
     class concurrent_queue
@@ -18,178 +23,316 @@ namespace rpp
         std::deque<T> Queue;
         mutable std::mutex Mutex;
         std::condition_variable Waiter;
+
     public:
-    
-        ~concurrent_queue()
+        concurrent_queue() = default;
+        ~concurrent_queue() noexcept
         {
             clear(); // safely lock, pop and notify all waiters to give up
         }
-        
-        bool empty() const { return Queue.empty(); }
-        int  size()  const { return (int)Queue.size(); }
-        int safe_size() const
+
+        /**
+         * @brief Returns the internal mutex for this queue
+         */
+        [[nodiscard]] std::mutex& sync() const noexcept { return Mutex; }
+
+        /** @returns TRUE if this queue is empty */
+        [[nodiscard]] bool empty() const noexcept { return Queue.empty(); }
+
+        /**
+         * @returns Approximate size of the queue (unsafe)
+         */
+        [[nodiscard]] int size() const noexcept { return (int)Queue.size(); }
+
+        /**
+         * @returns Synchronized current size of the queue,
+         *          however using this value is not atomic
+         */
+        [[nodiscard]] int safe_size() const noexcept
         {
-            std::lock_guard<std::mutex> lock {Mutex};
-            return (int)Queue.size();
+            if (Mutex.try_lock()) // @note try_lock() for noexcept guarantee
+            {
+                int sz = size();
+                Mutex.unlock();
+                return sz;
+            }
+            return size();
         }
-        
-        std::mutex& sync() const { return Mutex; }
-        void notify() { Waiter.notify_all(); }
-        
-        template<class ChangeWaitFlags>
-        void notify(const ChangeWaitFlags& changeFlags)
+
+        /**
+         * @brief Notifies all waiters that the queue has changed
+         */
+        void notify() noexcept
         {
-            { std::lock_guard<std::mutex> lock {Mutex};
-                changeFlags(); }
             Waiter.notify_all();
         }
 
-        void clear()
+        /**
+         * @brief Thread-safely clears the entire queue and notifies all waiters
+         */
+        void clear() noexcept
         {
-            { std::lock_guard<std::mutex> lock {Mutex};
-                Queue.clear(); }
-            Waiter.notify_all();
+            if (Mutex.try_lock()) // @note try_lock() for noexcept guarantee
+            {
+                Queue.clear();
+                Mutex.unlock();
+                notify();
+            }
         }
-        
+
+        /**
+         * @returns An atomic copy of the entire queue items
+         */
+        [[nodiscard]] std::deque<T> atomic_copy() const noexcept
+        {
+            std::deque<T> copy;
+            if (Mutex.try_lock()) // @note try_lock() for noexcept guarantee
+            {
+                copy = Queue;
+                Mutex.unlock();
+            }
+            return copy;
+        }
+
+        /**
+         * @brief Thread-safely modify wait condition in the changeWaitFlags callback
+         *        and then notifies all waiters.
+         *        If any other threads have locked the mutex, then modifying condition
+         *        flags could be illegal. This allows synchronizing to the queue state
+         *        to ensure all other threads are idle and have released the mutex.
+         * @details This is meant to be used with wait_pop() callback overload
+         *          where the wait_pop() is checking for a cancellation condition.
+         *          This allows for safely setting that condition from another thread
+         *          and notify all waiters.
+         * @code
+         *      queue.notify([this]{ this->atomic_cancelled = true; })
+         * @endcode
+         */
+        template<class ChangeWaitFlags>
+        void notify(const ChangeWaitFlags& changeWaitFlags)
+        {
+            if (Mutex.try_lock()) // @note try_lock() for noexcept guarantee
+            {
+                changeWaitFlags();
+                Mutex.unlock();
+                notify();
+            }
+        }
+
+        /**
+         * @brief Thread-safely moves an item into the queue and notifies all waiters
+         */
         void push(T&& item)
         {
-            { std::lock_guard<std::mutex> lock {Mutex};
-                Queue.emplace_back(std::move(item)); }
-            Waiter.notify_all();
+            {
+                std::lock_guard<std::mutex> lock {Mutex}; // may throw
+                Queue.emplace_back(std::move(item)); // may throw
+            }
+            notify();
         }
 
+        /**
+         * @brief Thread-safely copies an item into the queue and notifies all waiters
+         */
         void push(const T& item)
         {
-            { std::lock_guard<std::mutex> lock {Mutex};
-                Queue.push_back(item); }
-            Waiter.notify_all();
+            {
+                std::lock_guard<std::mutex> lock {Mutex}; // may throw
+                Queue.push_back(item); // may throw
+            }
+            notify();
         }
 
-        // pushes an item without calling notify_all()
+        /**
+         * @brief Thread-safely moves an item into the queue without notifying waiters
+         */
         void push_no_notify(T&& item)
         {
-            { std::lock_guard<std::mutex> lock {Mutex};
-                Queue.emplace_back(std::move(item)); }
+            {
+                std::lock_guard<std::mutex> lock {Mutex}; // may throw
+                Queue.emplace_back(std::move(item)); // may throw
+            }
         }
 
-        T pop()
+        /**
+         * @returns Thread-safely pops an item from the queue and notifies other waiters
+         * @throws runtime_error if the queue is empty
+         */
+        [[nodiscard]] T pop()
         {
             T item;
-            { std::lock_guard<std::mutex> lock {Mutex};
+            {
+                std::lock_guard<std::mutex> lock {Mutex};
                 if (Queue.empty()) throw std::runtime_error("concurrent_queue<T>::pop(): Queue was empty!");
-                pop_unlocked(item); }
-            Waiter.notify_all();
+                pop_unlocked(item);
+            }
+            notify();
             return item;
         }
 
-        std::deque<T> atomic_copy() const
-        {
-            std::lock_guard<std::mutex> lock {Mutex};
-            return Queue;
-        }
-
-        bool try_pop(T& outItem)
-        {
-            std::lock_guard<std::mutex> lock {Mutex};
-            if (Queue.empty())
-                return false;
-            pop_unlocked(outItem);
-            return true;
-        }
-
         /**
-         * Waits until an item is ready to be popped.
-         * The cancellationCondition is used to terminate the wait:
-         * @param outItem [out] The popped item. Only valid if return value is TRUE
-         * @param timeoutMillis Maximum number of milliseconds to wait
-         * @param cancellationCondition Cancellation condition, CANCEL if cancellationCondition()==true
-         * @param cancellationInterval Interval in milliseconds for checking cancellation condition
-        * @return TRUE if an item was popped,
-         *        FALSE if no item popped due to: timeout or cancellation
-         * @code
-         *   string item;
-         *   if (queue.wait_pop(item, [&]{ return Cancelled || Finished; })
-         *   {
-         *       // item is valid
-         *   }
-         * @endcode
-         */
-        template<class WaitUntil>
-        bool wait_pop(T& outItem, const WaitUntil& cancellationCondition,
-                      int cancellationInterval = 1000)
+         * @brief Attempts to pop an item from the queue without waiting
+         * @note try_pop() is excellent for polling scenarios if you don't
+         *       want to wait for an item, but just check if any work could be done, 
+         *       otherwise just continue
+         * @returns TRUE if an item was popped, FALSE if the queue was empty
+         */ 
+        [[nodiscard]] bool try_pop(T& outItem) noexcept
         {
-            std::chrono::milliseconds interval {cancellationInterval};
-            std::unique_lock<std::mutex> lock {Mutex};
-            
-            while (Queue.empty())
+            if (Mutex.try_lock())
             {
-                if (cancellationCondition())
-                    break;
-                Waiter.wait_for(lock, interval);
+                if (Queue.empty())
+                {
+                    Mutex.unlock();
+                    return false;
+                }
+                pop_unlocked(outItem);
+                Mutex.unlock();
+                return true;
             }
-            
-            if (Queue.empty())
-                return false;
-            pop_unlocked(outItem);
-            return true;
-        }
-
-        /**
-         * Waits up to N milliseconds until an item is ready to be popped.
-         * The cancellationCondition is used to terminate the wait:
-         * @param outItem [out] The popped item. Only valid if return value is TRUE
-         * @param timeoutMillis Maximum number of milliseconds to wait before returning FALSE
-         * @param cancellationCondition Cancellation condition, CANCEL if cancellationCondition()==true
-         * @return TRUE if an item was popped,
-         *         FALSE if no item popped due to: timeout or cancellation
-         * @code
-         *   string item;
-         *   if (queue.wait_pop(item, 1000, [&]{ return Cancelled || Finished; })
-         *   {
-         *       // item is valid
-         *   }
-         * @endcode
-         */
-        template<class WaitUntil>
-        bool wait_pop(T& outItem, int timeoutMillis, const WaitUntil& cancellationCondition)
-        {
-            auto end = std::chrono::system_clock::now()
-                     + std::chrono::milliseconds{timeoutMillis};
-            std::unique_lock<std::mutex> lock {Mutex};
-            
-            while (Queue.empty())
-            {
-                if (cancellationCondition())
-                    break;
-                std::cv_status status = Waiter.wait_until(lock, end);
-                if (status == std::cv_status::timeout)
-                    break;
-            }
-            
-            if (Queue.empty())
-                return false;
-            pop_unlocked(outItem);
-            return true;
+            return false;
         }
 
         /**
          * Waits forever until an item is ready to be popped.
          * @warning If no items are present, then this will deadlock!
+         * @note This is a convenience method for wait_pop() with no timeout
+         *       and is most convenient for producer/consumer threads.
+         *       @see test_concurrent_queue.cpp TestCase(basic_producer_consumer) for example usage.
          */
-        T wait_pop()
+        [[nodiscard]] T wait_pop() noexcept
         {
-            std::unique_lock<std::mutex> lock {Mutex};
-            
+            std::unique_lock<std::mutex> lock {Mutex}; // may throw
             while (Queue.empty())
                 Waiter.wait(lock);
-
             T result;
             pop_unlocked(result);
             return result;
         }
-        
+
+        /**
+         * Waits up to @param timeout duration until an item is ready to be popped.
+         * For waiting without timeout @see wait_pop().
+         * 
+         * @note This is best used for cases where you want to wait a specific
+         *       amount of time for an item to be available and don't want to
+         *       return before that time.
+         *       Useful for synchronization tasks that have a time limit.
+         * 
+         * @param outItem [out] The popped item. Only valid if return value is TRUE
+         * @param timeout [required] Time to wait before returning FALSE
+         * @return TRUE if an item was popped, FALSE if timed out.
+         * @code
+         *   string item;
+         *   if (queue.wait_pop(item, std::chrono::milliseconds{100}))
+         *   {
+         *       // item is valid
+         *   }
+         *   // else: timeout was reached
+         * @endcode
+         */
+        [[nodiscard]]
+        bool wait_pop(T& outItem, std::chrono::system_clock::duration timeout)
+        {
+            auto end = std::chrono::system_clock::now() + timeout;
+            std::unique_lock<std::mutex> lock {Mutex}; // may throw
+            while (Queue.empty())
+            {
+                std::cv_status status = Waiter.wait_until(lock, end); // may throw
+                if (status == std::cv_status::timeout)
+                    break;
+            }
+            if (Queue.empty())
+                return false;
+            pop_unlocked(outItem);
+            return true;
+        }
+
+        /**
+         * Waits up to @param timeout duration until an item is ready to be popped.
+         * The cancellationCondition is used to terminate the wait.
+         * 
+         * @note This is a wrapper around wait_pop_interval, with the cancellation check
+         *       interval set to 1/10th of the timeout.
+         * 
+         * @param outItem [out] The popped item. Only valid if return value is TRUE
+         * @param timeout [required] Maximum time to wait before returning FALSE
+         * @param cancellationCondition Cancellation condition, CANCEL if cancellationCondition()==true
+         * @return TRUE if an item was popped,
+         *         FALSE if no item popped due to: timeout or cancellation.
+         * @code
+         *   string item;
+         *   auto timeout = std::chrono::milliseconds{100};
+         *   if (queue.wait_pop(item, timeout, [this]{ return this->cancelled || this->finished; })
+         *   {
+         *       // item is valid
+         *   }
+         * @endcode
+         */
+        template<class WaitUntil>
+        [[nodiscard]]
+        bool wait_pop(T& outItem, std::chrono::system_clock::duration timeout,
+                      const WaitUntil& cancellationCondition)
+        {
+            auto interval = timeout / 10;
+            return wait_pop_interval<WaitUntil>(outItem, timeout, interval,
+                                                cancellationCondition);
+        }
+
+        /**
+         * Waits until an item is ready to be popped.
+         * The @param cancellationCondition is used to terminate the wait.
+         * And @param conditionInterval defines how often the cancellationCondition is checked.
+         * 
+         * @note This is a superior alternative to wait_pop(), because the cancellationCondition
+         *       is checked multiple times, instead of only between waits if someone notifies.
+         * 
+         * @param outItem [out] The popped item. Only valid if return value is TRUE
+         * @param timeout Total timeout for the entire wait loop, granularity is defined by @param interval
+         * @param interval Interval for checking the cancellation condition
+         * @param cancellationCondition Cancellation condition, CANCEL if cancellationCondition()==true
+         * @return TRUE if an item was popped,
+         *         FALSE if no item popped due to: timeout or cancellation
+         * @code
+         *   string item;
+         *   auto interval = std::chrono::milliseconds{100};
+         *   if (queue.wait_pop_interval(item, [this]{ return this->cancelled || this->finished; })
+         *   {
+         *       // item is valid
+         *   }
+         * @endcode
+         */
+        template<class WaitUntil>
+        [[nodiscard]]
+        bool wait_pop_interval(T& outItem,
+                               std::chrono::system_clock::duration timeout,
+                               std::chrono::system_clock::duration interval,
+                               const WaitUntil& cancellationCondition)
+        {
+            auto next = std::chrono::system_clock::now();
+            auto end = next + timeout;
+            std::unique_lock<std::mutex> lock {Mutex};
+            while (Queue.empty())
+            {
+                if (cancellationCondition())
+                    break;
+                next += interval;
+                if (next > end)
+                    next = end;
+                std::cv_status status = Waiter.wait_until(lock, next); // may throw
+                if (status == std::cv_status::no_timeout && !Queue.empty())
+                    break; // notified
+                if (next == end)
+                    break; // final timeout
+            }
+            if (Queue.empty())
+                return false;
+            pop_unlocked(outItem);
+            return true;
+        }
+
     private:
-        inline void pop_unlocked(T& outItem)
+        inline void pop_unlocked(T& outItem) noexcept
         {
             outItem = std::move(Queue.front());
             Queue.pop_front();
--- tests/test_concurrent_queue.cpp
+++ tests/test_concurrent_queue.cpp
@@ -1,7 +1,9 @@
 #include <rpp/concurrent_queue.h>
 #include <rpp/future.h>
+#include <rpp/timer.h>
 #include <rpp/tests.h>
 using namespace rpp;
+using namespace std::chrono_literals;
 
 TestImpl(test_concurrent_queue)
 {
@@ -9,7 +11,32 @@ TestImpl(test_concurrent_queue)
     {
     }
 
-    TestCase(basic_enqueue_dequeue)
+    TestCase(push_and_pop)
+    {
+        concurrent_queue<std::string> queue;
+        queue.push("item1");
+        queue.push("item2");
+        std::string item3 = "item3";
+        queue.push(item3); // copy
+        AssertThat(queue.size(), 3);
+        AssertThat(queue.safe_size(), 3);
+        AssertThat(queue.empty(), false);
+
+        AssertThat(queue.pop(), "item1");
+        AssertThat(queue.pop(), "item2");
+        AssertThat(queue.pop(), "item3");
+        AssertThat(queue.size(), 0);
+        AssertThat(queue.safe_size(), 0);
+        AssertThat(queue.empty(), true);
+
+        queue.push_no_notify("item4");
+        AssertThat(queue.size(), 1);
+        AssertThat(queue.safe_size(), 1);
+        AssertThat(queue.empty(), false);
+        AssertThat(queue.pop(), "item4");
+    }
+
+    TestCase(clear)
     {
         concurrent_queue<std::string> queue;
         queue.push("item1");
@@ -17,15 +44,53 @@ TestImpl(test_concurrent_queue)
         queue.push("item3");
         AssertThat(queue.size(), 3);
         AssertThat(queue.empty(), false);
-
-        AssertThat(queue.pop(), "item1");
-        AssertThat(queue.pop(), "item2");
-        AssertThat(queue.pop(), "item3");
+        queue.clear();
         AssertThat(queue.size(), 0);
         AssertThat(queue.empty(), true);
     }
 
-    TestCase(basic_producer_consumer)
+    TestCase(atomic_copy)
+    {
+        concurrent_queue<std::string> queue;
+        queue.push("item1");
+        queue.push("item2");
+        queue.push("item3");
+        std::deque<std::string> items = queue.atomic_copy();
+        AssertThat(items.size(), 3);
+        AssertThat(items.at(0), "item1");
+        AssertThat(items.at(1), "item2");
+        AssertThat(items.at(2), "item3");
+    }
+
+    // try_pop() is excellent for polling scenarios
+    // if you don't want to wait for an item, but just check
+    // if any work could be done, otherwise just continue
+    TestCase(try_pop)
+    {
+        concurrent_queue<std::string> queue;
+        std::string item;
+        AssertThat(queue.try_pop(item), false);
+        AssertThat(item, "");
+
+        queue.push("item1");
+        AssertThat(queue.try_pop(item), true);
+        AssertThat(item, "item1");
+        AssertThat(queue.try_pop(item), false);
+        AssertThat(item, "item1");
+
+        queue.push("item2");
+        queue.push("item3");
+        AssertThat(queue.try_pop(item), true);
+        AssertThat(item, "item2");
+        AssertThat(queue.try_pop(item), true);
+        AssertThat(item, "item3");
+        AssertThat(queue.try_pop(item), false);
+        AssertThat(item, "item3");
+    }
+
+    // wait_pop() is best used for producer/consumer scenarios
+    // for long-lived service threads that don't have any cancellation mechanism
+    TestCase(wait_pop_producer_consumer)
     {
         concurrent_queue<std::string> queue;
 
@@ -43,5 +108,137 @@ TestImpl(test_concurrent_queue)
 
         producer.get();
         consumer.get();
+    }
+
+    // 
+    TestCase(wait_pop_with_timeout)
+    {
+        concurrent_queue<std::string> queue;
+        std::string item;
+        AssertThat(queue.wait_pop(item, 5ms), false);
+        AssertThat(queue.wait_pop(item, 0ms), false);
+
+        // if someone pushes an item if we have a huge timeout, 
+        // we should get it immediately
+        queue.push("item1");
+        rpp::Timer t;
+        AssertThat(queue.wait_pop(item, 10s), true);
+        AssertLess(t.elapsed_ms(), 10);
+        AssertThat(queue.wait_pop(item, 15ms), false);
+    }
+
+    // introduce a slow producer thread so we can test our timeouts
+    TestCase(wait_pop_with_timeout_slow_producer)
+    {
+        concurrent_queue<std::string> queue;
+        cfuture<void> slow_producer = rpp::async_task([&] {
+            std::this_thread::sleep_for(50ms);
+            queue.push("item1");
+            std::this_thread::sleep_for(50ms);
+            queue.push("item2");
+            std::this_thread::sleep_for(50ms);
+            queue.push("item3");
+            std::this_thread::sleep_for(100ms);
+            queue.push("item3");
+        });
+
+        std::string item;
+        AssertThat(queue.wait_pop(item, 5ms), false);
+        AssertThat(queue.wait_pop(item, 0ms), false);
+        AssertThat(queue.wait_pop(item, 5ms), false);
+        AssertThat(queue.wait_pop(item, 50ms), true); // this should not timeout
+        AssertThat(item, "item1");
+        rpp::Timer t;
+        AssertThat(queue.wait_pop(item, 75ms), true);
+        AssertLess(t.elapsed_ms(), 55);
+        AssertThat(item, "item2");
+        AssertThat(queue.wait_pop(item, 75ms), true);
+        AssertThat(item, "item3");
+        // now we enter a long wait, but we should be notified by the producer
+        t.start();
+        AssertThat(queue.wait_pop(item, 1000ms), true);
+        AssertLess(t.elapsed_ms(), 110);
+    }
+
+    // in general the pop_with_timeout is not very useful because
+    // it requires some external mechanism to notify the queue
+    TestCase(wait_pop_with_timeout_and_cancelcondition)
+    {
+        concurrent_queue<std::string> queue;
+        std::atomic_bool finished = false;
+        cfuture<void> slow_producer = rpp::async_task([&] {
+            std::this_thread::sleep_for(50ms);
+            queue.push("item1");
+            std::this_thread::sleep_for(50ms);
+            queue.push("item2");
+            std::this_thread::sleep_for(50ms);
+            queue.push("item3");
+            std::this_thread::sleep_for(50ms);
+            finished = true;
+            queue.notify(); // notify any waiting threads
+        });
+
+        auto cancelCondition = [&] { return (bool)finished; };
+        std::string item;
+        AssertThat(queue.wait_pop(item, 1ms, cancelCondition), false); // this should timeout
+        AssertThat(queue.wait_pop(item, 50ms, cancelCondition), true);
+        AssertThat(item, "item1");
+        AssertThat(queue.wait_pop(item, 51ms, cancelCondition), true);
+        AssertThat(item, "item2");
+        AssertThat(queue.wait_pop(item, 51ms, cancelCondition), true);
+        AssertThat(item, "item3");
+        // now wait until producer exits by setting the cancellation condition
+        // this should not take longer than ~55ms
+        rpp::Timer t;
+        AssertThat(queue.wait_pop(item, 1000ms, cancelCondition), false);
+        AssertLess(t.elapsed_ms(), 55);
+    }
+
+    // ensure that wait_pop_interval actually checks the cancelCondition
+    // with sufficient frequency
+    TestCase(wait_pop_interval)
+    {
+        concurrent_queue<std::string> queue;
+        cfuture<void> slow_producer = rpp::async_task([&] {
+            std::this_thread::sleep_for(50ms);
+            queue.push("item1");
+            std::this_thread::sleep_for(50ms);
+            queue.push("item2");
+            std::this_thread::sleep_for(50ms);
+            queue.push("item3");
+        });
+
+        // wait for 100ms with 10ms intervals, but first item will arrive within ~50ms
+        std::string item;
+        std::atomic_int counter = 0;
+
+        AssertTrue(queue.wait_pop_interval(item, 100ms, 10ms, [&] { return ++counter >= 10; }));
+        AssertThat(item, "item1");
+        AssertGreaterOrEqual(int(counter), 5);
+        AssertLess(int(counter), 10);
+
+        // wait for 20ms with 5ms intervals, it should timeout
+        counter = 0;
+        AssertFalse(queue.wait_pop_interval(item, 20ms, 5ms, [&] { return ++counter >= 10; }));
+        AssertGreaterOrEqual(int(counter), 4);
+        AssertLess(int(counter), 6);
+
+        // wait another 30ms with 2ms intervals, and it should trigger the cancelcondition
+        counter = 0;
+        AssertFalse(queue.wait_pop_interval(item, 30ms, 2ms, [&] { return ++counter >= 10; }));
+        AssertThat(int(counter), 10); // it should have cancelled exactly at 10 checks
+
+        // wait until we pop the item finally
+        counter = 0;
+        AssertTrue(queue.wait_pop_interval(item, 100ms, 10ms, [&] { return ++counter >= 10; }));
+        AssertThat(item, "item2");
+        AssertLess(int(counter), 10); // we should never have reached 10 checks
+
+        // now wait with extreme short intervals
+        counter = 0;
+        AssertTrue(queue.wait_pop_interval(item, 55ms, 1ms, [&] { return ++counter >= 55; }));
+        AssertThat(item, "item3");
+        AssertLess(int(counter), 55); // we should never have reached the limit
+        AssertGreater(int(counter), 49); // but we should have reached at least 49 checks
     }
 };
