6fc8a211292876f1650e7f4977ab9f09cbbf6c60
Oleh Klepatskyi <o.klepatskyi@gmail.com>
2024-05-02 10:51:43 +0300

    Add stop method to Timer

--- src/rpp/timer.h
+++ src/rpp/timer.h
@@ -254,6 +254,9 @@ namespace rpp
         /** Starts the timer */
         void start() noexcept { started = TimePoint::now(); }
 
+        /** Stops the timer */
+        void stop() noexcept { started = TimePoint::zero(); }
+
         /** @brief Resets the timer to a custom time point */
         void reset(const TimePoint& time) noexcept { started = time; }
 
